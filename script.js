beb0379a391a829303a5ce1871a4dbadc8189c81
Vaibhav Sharma <92776886+Vaibhav992@users.noreply.github.com>
2026-03-01 21:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get current URL
+var currentURL = location.href;
+
+// Get length of URL
+var lengthOfURL = currentURL.length;
+
+// Show result
+alert("The length of the URL is: " + lengthOfURL);
